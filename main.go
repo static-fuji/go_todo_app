165396063f968867ca55b3a8d37d3a90e279d774
static-fuji <jyun1228haru@icloud.com>
2024-11-08 17:07:13 +0900

    add: シグナルハンドリング機能を追加

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/static-fuji/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +23,9 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -32,6 +38,7 @@ func run(ctx context.Context) error {
 	log.Printf("start with %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
